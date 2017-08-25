af0a38c9805b504e53de885f4b3f4e17e48718ed
FirdousG <30408056+FirdousG@users.noreply.github.com>
2017-08-25 12:40:08 +0100

    20170825-01

---
+++ testcsini.cmd
@@ -0,0 +1,3 @@
+﻿del C:\MININT\SMSOSD\OSDLOGS\VARIABLES.DAT /q
+cscript ZTIGather.wsf /inifile:E:\PackagesSource\Microsoft\MDTSettings\01\CustomSettings.ini
+“E:\BuildLogs\CMTrace.exe” c:\minint\smsosd\osdlogs\bdd.log
